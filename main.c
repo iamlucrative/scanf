9b5657b52162d47024a48325f3abbe1f7ded95dd
iamlucrative <123393141+iamlucrative@users.noreply.github.com>
2024-02-20 20:09:12 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a,b,c,d ;
+   printf("Enter the first value:");
+   scanf("%d",&a);
+   printf("Enter the second value:");
+   scanf("%d", &b);
+   printf("Enter the third value:");
+   scanf("%d" ,&c);
+   d=a+b+c ;
+   printf("%d+%d+%d=%d",a,b,c,d);
+
+    return 0;
+}
